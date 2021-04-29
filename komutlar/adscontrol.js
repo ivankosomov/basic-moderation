6e591543d2c39007a5053681d1c2af7e364b615c
Ivan Kosomov <83186495+ivankosomov@users.noreply.github.com>
2021-04-29 20:06:20 +0300

    Create adscontrol.js

---
+++ komutlar/adscontrol.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ayarlar = require("../ayarlar.json");
+let prefix = ayarlar.prefix;
+
+exports.run = (client, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.reply(
+      `Bu Komutu Kullanabilmek İçin **Mesajları Yönet** İznine Sahip Olmalısın!`
+    );
+//Çok sıkılıom dostlar çook
+  const members = message.guild.members.cache.filter(
+    member =>
+      member.user.presence.game &&
+      /(discord|http|.com|.net|.org|invite|İnstagram|Facebook|watch|Youtube|youtube|facebook|instagram)/g.test(
+        member.user.presence.game.name
+      )
+  );
+  const memberss = message.guild.members.cache.filter(
+    member =>
+      member.user.username &&
+      /(discord|http|.com|.net|.org|invite|İnstagram|Facebook|watch|Youtube|youtube|facebook|instagram)/g.test(
+        member.user.username
+      )
+  );
+
+  const embed = new Discord.MessageEmbed()
+
+    .addField(
+      "Oynuyor Mesajı Reklam İçeren Kullanıcılar :",
+      members
+        .map(member => `${member} = ${member.user.presence.game.name}`)
+        .join("\n") || "Kimsenin Oynuyor Mesajı Reklam İçermiyor."
+    )
+    .addField(
+      "Kullanıcı Adı Reklam İçeren Kullanıcılar :",
+      memberss
+        .map(member => `${member} = ${member.user.username}`)
+        .join("\n") || "Kimsenin Kullanıcı Adı Reklam İçermiyor."
+    )
+    .setColor("GREEN");
+
+  message.channel.send({ embed });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["ads", "reklamara", "reklamtaraması"],
+  permLevel: 2
+};
+
+exports.help = {
+  name: "reklam-taraması",
+  description: "Reklam ya",
+  usage: "reklam-taraması"
+};
